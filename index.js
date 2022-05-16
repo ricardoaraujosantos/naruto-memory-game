8b693e0d6ff5d3850cede377a9abb801a04dfd63
Ricardo <ricardoaraujo3713@gmail.com>
2022-05-15 21:49:53 -0300

    acertando bugs dos cards

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+let btnMyName = document.getElementById('btnMyName');
+let inputWriteName = document.getElementById('writeMyName');
+
+function definesName() {
+    let promptName = prompt('Por favor, Digite seu nome!');
+    localStorage.setItem('Name', promptName);
+    let nameSaved = localStorage.getItem('Name');
+    
+    if(!localStorage.getItem('Name') || !promptName){
+        definesName();
+    }else{  
+        inputWriteName.innerHTML = 'Olá, Bem Vindo ' + nameSaved;
+        btnMyName.innerHTML = `Não é ${nameSaved}? Click!`
+    }
+}
+
+window.addEventListener('load', function() {
+    let localStorageName = localStorage.getItem('Name');
+    if(localStorageName !== null){
+        inputWriteName.innerHTML = 'Olá, Bem Vindo ' + localStorageName;
+        btnMyName.innerHTML = `Não é ${localStorageName}? Click!`
+    }
+});
+
+btnMyName.addEventListener('click', definesName);
+
 const imgcards= [
     'gaara.jpg',
     'itachi.jpg',
@@ -13,8 +39,8 @@ let cardsHtml = '';
 imgcards.forEach(img => {
     cardsHtml += `
         <div id= "card" class= "my-card" data-card= "${img}">
-            <img id="cardFront" class="back" src="imagens/card-verso2.jpg" alt="frente da carta" />
-            <img id="cardback" class="front" src="imagens/${img}"  alt="verso da carta" />
+            <img id="cardFront" class="back" src="imagens/card-verso.jpg" alt="verso da carta" />
+            <img id="cardback" class="front" src="imagens/${img}"  alt="frente da carta" />
         </div>
     `
 })
@@ -25,31 +51,34 @@ cardsWrapper.innerHTML = cardsHtml + cardsHtml;
 let cards = document.querySelectorAll('.my-card');
 cards.forEach(elemCard => elemCard.addEventListener('click', rotateCard));
 
+function shuffleCards() {
+    cards.forEach(card => {
+      let ramdomPosition = Math.floor(Math.random() * 12);
+      card.style.order = ramdomPosition;
+    });
+  };
+ shuffleCards();
+
 let firstCard, secondCard;
 let block = false;
 
 function rotateCard() {
    if(block) return false;
-
-   this.classList.toggle('card-virar')
-   if(!firstCard){
+   if(this === firstCard) return false;
+   
+   this.classList.toggle('card-virar');
+   
+   if(!firstCard){ 
        firstCard = this;
        return false;
    }
    secondCard = this;
-   hitThwoSameCards();
+   checkMatchingCards();
 };
 
-const myScore = document.getElementById('myScore');
-let score = 0;
-
-function hitThwoSameCards() {
+function checkMatchingCards() {
     const sameCards = firstCard.getAttribute('data-card') === secondCard.getAttribute('data-card');
-    !sameCards ? desableCards() : desableClickCards();
-    if(sameCards){
-        myScore.innerHTML = score += 10;
-        return false;
-    }
+    !sameCards ? desableCards() : scoreAndClearEvent(); 
 }
 
 function desableCards() {
@@ -57,16 +86,18 @@ function desableCards() {
     setTimeout(() => {
         firstCard.classList.remove('card-virar');
         secondCard.classList.remove('card-virar');
-
         [firstCard, secondCard, block]= [null, null, false]
     }, 3000)
-     
 }
 
-function desableClickCards (){
+const myScore = document.getElementById('myScore');
+let score = 0;
+
+function scoreAndClearEvent(){
+    myScore.innerHTML = `Score: ${score += 100}`;
+
     firstCard.removeEventListener('click', rotateCard);
     secondCard.removeEventListener('click', rotateCard);
-
     [firstCard, secondCard, block]= [null, null, false];
     
 }
